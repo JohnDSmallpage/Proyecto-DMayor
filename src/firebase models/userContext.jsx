e470aa3373dbd5ac49335759be914ea4851414ef
GabrielSuarez <suarezgabrielr@gmail.com>
2023-05-17 13:52:46 -0400

    login y registro

---
+++ src/firebase models/userContext.jsx
@@ -0,0 +1,37 @@
+import React,{useContext,createContext,useEffect,useState} from "react";
+import {onAuthStateChanged} from "firebase/auth"
+import { auth } from "./Config";
+export const UserContext = createContext(null);
+
+export function UserContextProvider({ children }){
+const [user,setUser] =useState(null)
+const [isLoading,setIsLoading] = useState(true)
+
+useEffect(()=>{
+    onAuthStateChanged(auth ,async (firebaseUser)=>{
+        setIsLoading(true)
+        if(firebaseUser){
+            // const profile = await getUserProfile(firebaseUser.email)
+            setUser(firebaseUser);
+            // console.log(profile)
+        }else{
+           setUser(null); 
+        }
+        setIsLoading(false)
+    });
+},[]);
+
+    return( <UserContext.Provider 
+    value={{
+        user,
+        setUser,
+        isLoading,
+    }}>
+        {children}
+    </UserContext.Provider>
+    )
+}
+
+export function useUser(){
+    return useContext(UserContext);
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import {BrowserRouter} from 'react-router-dom';
 import {Routes, Route} from 'react-router-dom';
-import {LANDING_PAGE, CATEGORIAS, PROVEEDORES, FAQ, DEFAULT} from './routes/Url';
+import {LANDING_PAGE, CATEGORIAS, PROVEEDORES, FAQ, DEFAULT, REGISTER, LOGIN} from './routes/Url';
 import {LandingPage} from './views/LandingPage';
 import {Layout} from './views/Layout';
 import {Providers} from './views/Providers';
 import {Faq} from './views/Faq';
 import { Error } from './views/Error';
+import {RegisterPage} from './views/RegisterPage'
+import { LoginPage } from './views/LoginPage';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -21,6 +23,8 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           <Route path={CATEGORIAS} element={<Providers/>} />    
           <Route path={FAQ} element={<Faq/>} />
           <Route path={DEFAULT} element={<Error/>} />
+          <Route path={REGISTER} element={<RegisterPage/>}/>
+          <Route path={LOGIN} element={<LoginPage/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
