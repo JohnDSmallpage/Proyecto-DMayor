f5e19d8ae7f46e7340c4c1013fbc39a42f7ad185
SebasAure <saure@correo.unimet.edu.ve>
2023-05-22 18:48:52 -0400

    arreglos interfaz supplierprofile

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,7 @@ import './index.css'
 import {BrowserRouter} from 'react-router-dom';
 import {Routes, Route} from 'react-router-dom';
 
-<<<<<<< HEAD
 import {LANDING_PAGE, CATEGORIAS, PROVEEDORES, FAQ, DEFAULT, SEARCH_PAGE, REGISTER, LOGIN, APPLICATION, ADMIN, PRODUCT_PAGE,SUPPLIER_PROFILE, CLIENT_PROFILE, CHECKOUT, CHAT} from './routes/Url';
-=======
-import {LANDING_PAGE, CATEGORIAS, PROVEEDORES, FAQ, DEFAULT, SEARCH_PAGE, REGISTER, LOGIN, APPLICATION, ADMIN, PRODUCT_PAGE,SUPPLIER_PROFILE, CATALOGO} from './routes/Url';
->>>>>>> 7801a758b4b80294c0c624af4f45e61cfca59a94
 import {LandingPage} from './views/LandingPage';
 import {Layout} from './views/Layout';
 import {Providers} from './views/Providers';
@@ -22,7 +18,6 @@ import { PrivateRoute } from './components/PrivateRoute';
 import { AdminView } from './views/adminView';
 import { ProductPage } from './views/ProductPage';
 import {SupplierProfile} from './views/SupplierProfile';
-<<<<<<< HEAD
 import { PrivateSupplier } from './components/PrivateSupplier';
 import { ClientProfile } from './views/ClientProfile';
 import { PrivateRouteClient } from './components/PrivateRouteClient';
@@ -35,9 +30,6 @@ import { SupplierCatalog } from './views/SupplierCatalog';
 import { SUPPLIER_INFO } from './routes/Url';
 import { SupplierInfo } from './views/SupplierInfo';
 
-=======
-import {Catalogo} from './views/Catalogo';
->>>>>>> 7801a758b4b80294c0c624af4f45e61cfca59a94
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -55,7 +47,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           <Route path={APPLICATION} element={<ApplicationPage/>}/>
           <Route path={ADMIN} element={<PrivateRoute><AdminView/></PrivateRoute>}/>
           <Route path={PRODUCT_PAGE} element={<ProductPage/>} />
-<<<<<<< HEAD
           <Route path={SUPPLIER_PROFILE} element={<PrivateSupplier><SupplierProfile/></PrivateSupplier>} />
           <Route path={CHECKOUT} element={<PrivateRouteClient> <CheckoutPage/> </PrivateRouteClient>} /> 
           <Route path={CLIENT_PROFILE} element={<PrivateRouteClient><ClientProfile/></PrivateRouteClient>}/>
@@ -64,10 +55,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
           <Route path={CATALOG} element={<PrivateSupplier><SupplierCatalog/></PrivateSupplier>}/>
           <Route path={SUPPLIER_INFO} element={<PrivateSupplier><SupplierInfo/></PrivateSupplier>}/>
 
-=======
-          <Route path={SUPPLIER_PROFILE} element={<SupplierProfile/>} />
-          <Route path={CATALOGO} element={<Catalogo/>} />
->>>>>>> 7801a758b4b80294c0c624af4f45e61cfca59a94
         </Route>
       </Routes>
     </BrowserRouter>
--- src/routes/Url.jsx
+++ src/routes/Url.jsx
@@ -10,13 +10,9 @@ export const APPLICATION ="/application";
 export const ADMIN = "/admin";
 export const PRODUCT_PAGE = "/product/:id";
 export const SUPPLIER_PROFILE = "/supplierProfile";
-<<<<<<< HEAD
 export const CHECKOUT = "/checkout";
 export const CLIENT_PROFILE = "/clientProfile"
 export const CHAT = "/chat"
 export const ADD_PRODUCT = "/addproduct"
 export const CATALOG = "/catalog"
 export const SUPPLIER_INFO = "/supplierInfo"
-=======
-export const CATALOGO = "/catalogo";
->>>>>>> 7801a758b4b80294c0c624af4f45e61cfca59a94
