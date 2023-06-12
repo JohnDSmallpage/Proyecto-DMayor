b05303fdd69aff373df487c5228d3948d0a67005
JohnDSmallpage <john.smallpage@correo.unimet.edu.ve>
2023-06-12 09:43:37 -0400

    añadido cantidad disponible a usuario proveedor

--- src/views/ProductPage.jsx
+++ src/views/ProductPage.jsx
@@ -212,6 +212,7 @@ export function ProductPage() {
                   Eliminar producto
                 </button>
 
+
                 <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
                   <svg
                     fill="currentColor"
@@ -227,7 +228,8 @@ export function ProductPage() {
               </div>
 
               
-              <span className="text-left whitespace-pre-line title-font font-medium text-xl text-gray-900">
+              <span className="flex flex-col mt-4 text-left whitespace-pre-line title-font font-medium text-xl text-gray-900 leading-normal">
+                Cantidad disponible: {product.availableQuantity} {" " + product.unity}
                 {product.discounts == [] ? (
                   <div>
                     <p>Loading...</p>
