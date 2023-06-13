a24ae45a4bc75705d34bf804494fbc5ce377d15f
Jennifer Pérez <jennifer.perez@correo.unimet.edu.ve>
2023-06-13 04:28:47 -0400

    .

--- src/components/Chat/Chats.jsx
+++ src/components/Chat/Chats.jsx
@@ -36,17 +36,6 @@ const Chats = () => {
   return (
     <div className='chats'>
      
-      {Object.entries(chats)?.sort((a,b)=>a[1].date - b[1].date).map(chat =>(
-        <div className="userChat" key={chat[0]} onClick={() =>handleSelect(chat[1].userInfo)}>
-          <img src={p} alt="" />
-          <div className="userChatInfo">
-            <span>{chat[1].userInfo.name}</span>
-            <p>{chat[1].lastMessage?.text}</p>
-          </div>
-        </div>
-        
-      ))}
-
     </div>
   )
 }
