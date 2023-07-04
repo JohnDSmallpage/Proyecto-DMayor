4675ce1f75bcf44d4bf19fcaa9eca604dc756f41
GabrielSuarez <suarezgabrielr@gmail.com>
2023-07-04 01:00:41 -0400

    tarjetas de feedback

--- src/firebase models/Config.js
+++ src/firebase models/Config.js
@@ -7,23 +7,14 @@ import {getStorage} from "firebase/storage"
 
 
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyCJoOaknb9ouhkLMxI_wmTSmLz6F_yyn5s",
-//     authDomain: "d-mayor-ad6ee.firebaseapp.com",
-//     projectId: "d-mayor-ad6ee",
-//     storageBucket: "d-mayor-ad6ee.appspot.com",
-//     messagingSenderId: "94552483401",
-//     appId: "1:94552483401:web:1f55cf1bebad8fe4cfc198"
-// };
-
 const firebaseConfig = {
-    apiKey: "AIzaSyD5999bJPY8rGnjDr0d7NMfEMbTJA2QCRA",
-    authDomain: "copia-8df64.firebaseapp.com",
-    projectId: "copia-8df64",
-    storageBucket: "copia-8df64.appspot.com",
-    messagingSenderId: "565136626228",
-    appId: "1:565136626228:web:27c0fad30fe0f54aeeb061"
-  };
+  apiKey: "AIzaSyCGXZJd80SzI56pcqt1uapUaE4gGLYOzV4",
+  authDomain: "d-mayor2.firebaseapp.com",
+  projectId: "d-mayor2",
+  storageBucket: "d-mayor2.appspot.com",
+  messagingSenderId: "405765491894",
+  appId: "1:405765491894:web:d5ed7202690111258d6a17"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
