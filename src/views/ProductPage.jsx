45ad5a91c1233dca35ca3d8cb0c5fa1dfc506b0d
SebasAure <saure@correo.unimet.edu.ve>
2023-07-03 23:41:07 -0400

    vistas catalogo y añadir producto listas

--- src/views/ProductPage.jsx
+++ src/views/ProductPage.jsx
@@ -189,7 +189,7 @@ export function ProductPage() {
                 }
               />
               <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-                <h2 className="text-sm title-font text-gray-500 tracking-widest">
+                <h2 className="text-md title-font text-gray-500 tracking-widest">
                   {" "}
                   {product.length == [] ? (
                     <div>
--- src/views/SupplierCatalog.jsx
+++ src/views/SupplierCatalog.jsx
@@ -13,7 +13,7 @@ import { getHiddenProductsBySupplier } from "../firebase models/user-service";
 
 export function SupplierCatalog() {
   const [products, setProducts] = useState([]);
-  const [hiddenProducts, setHiddenProducts] = useState([]);	
+  const [hiddenProducts, setHiddenProducts] = useState([]);
   const { user } = useUser();
 
   const productSearched = useContext(searchContext);
@@ -23,7 +23,6 @@ export function SupplierCatalog() {
     const data = await getProductsBySupplier(idProducts);
     setProducts(data);
   };
-
 
   const getSupplierHiddenProducts = async () => {
     const idProducts = user.catalog;
@@ -38,49 +37,53 @@ export function SupplierCatalog() {
   }, [products]);
 
   return (
-    <>
-      <h1 className="text-[20px] font-bold text-gray-800 text-center mt-2">
-        Catálogo
-      </h1>
+    <div id="main-container" className="flex justify-center items-center">
+      <div
+        id="internal-container"
+        className="my-5 mx-5 flex flex-col justify-center gap-[10px]"
+      >
+        <div id="top-container" className="flex flex-col gap-2">
+          <h1 className="text-2xl font-semibold ml-[20px]">Catálogo</h1>
 
-      <div className="flex justify-center mb-3 mt-2">
-        <Link
-          to={ADD_PRODUCT}
-          className="py-3 font-medium text-white bg-green-600 hover:bg-green-400 rounded-lg border-indigo-500 hover:shadow inline-flex space-x-2 items-center justify-center"
-        >
-          Añadir nuevo producto
-        </Link>
+          <div className="flex flex-wrap gap-5">
+            {products == null ? (
+              <div>No hay resultados para su búsqueda</div>
+            ) : (
+              products?.map((product, idx) => (
+                <>
+                  <Product info={product} key={idx} />
+                </>
+              ))
+            )}
+          </div>
+
+          <div className="flex justify-center mb-3 mt-2">
+            <Link
+              to={ADD_PRODUCT}
+              className="p-3 font-medium text-white bg-green-600 rounded-lg border-indigo-500 hover:shadow inline-flex space-x-2 items-center justify-center"
+            >
+              Añadir nuevo producto
+            </Link>
+          </div>
+        </div>
+        <div id="bottom-container" className="flex flex-col gap-2">
+          <h1 className="text-2xl font-semibold ml-[20px]">
+            Productos ocultos
+          </h1>
+
+          <div className="flex flex-wrap">
+            {hiddenProducts == null ? (
+              <div>No hay productos ocultos</div>
+            ) : (
+              hiddenProducts?.map((product, idx) => (
+                <>
+                  <Product info={product} key={idx} />
+                </>
+              ))
+            )}
+          </div>
+        </div>
       </div>
-
-      <div className="flex flex-wrap">
-        {products == null ? (
-          <div>No hay resultados para su búsqueda</div>
-        ) : (
-          products?.map((product, idx) => (
-            <>
-              <Product info={product} key={idx} />
-            </>
-          ))
-        )}
-      </div>
-
-      <h1 className="text-[20px] font-bold text-gray-800 text-center mt-3">
-        Productos ocultos
-      </h1>
-
-      <div className="flex flex-wrap">
-        {hiddenProducts == null ? (
-          <div>No hay productos ocultos</div>
-        ) : (
-          hiddenProducts?.map((product, idx) => (
-            <>
-              <Product info={product} key={idx} />
-            </>
-          ))
-        )}
-      </div>
-
-
-    </>
+    </div>
   );
 }
